70d4344d78af79d154c411d30c784a28974d850b
Medi Mule <medhanitmulatu6796@gmail.com>
2025-12-24 10:45:09 +0300

    deploying on netlify

--- Amazon-Clone/src/Api/axios.js
+++ Amazon-Clone/src/Api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-d94b3/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-d94b3/us-central1/api",
+  baseURL: "https://amazon-api-deploy-1-ptnz.onrender.com",
 });
 export {axiosInstance}
