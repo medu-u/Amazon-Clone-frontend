b95d47272db6afb7f79f0ec63db549fd35290d64
Medi Mule <medhanitmulatu6796@gmail.com>
2025-12-25 09:08:20 +0300

    style added and cart removal works

--- Amazon-Clone/src/Utility/reducer.js
+++ Amazon-Clone/src/Utility/reducer.js
@@ -38,7 +38,7 @@ export const reducer = (state, action) => {
       if (index >= 0) {
         if (newBasket[index].amount > 1) {
           newBasket[index] = {
-            ...newBasket(index),
+            ...newBasket[index],
             amount: newBasket[index].amount - 1,
           };
         } else {
